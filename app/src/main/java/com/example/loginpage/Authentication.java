103e879735b17b27f8f97340bd60531b2646c09b
aayalacdv <aayalacdv2000@gamil.com>
2021-05-24 19:39:05 +0200

    falta por arreglar constraints

---
+++ app/src/main/java/com/example/loginpage/Authentication.java
@@ -0,0 +1,19 @@
+package com.example.loginpage;
+
+import com.example.loginpage.models.Usuario;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface Authentication {
+    @POST("login/")
+    Call<ResponseBody>loginUser(@Body Usuario user);
+
+    @POST("signup/")
+    Call<ResponseBody>signUpUser(@Body Usuario user);
+}
+
